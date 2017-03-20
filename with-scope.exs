987731fde97d68929fa04c5c44b63ceff288cd80
Fred Allen <edwin.allen@gmail.com>
2017-03-19 23:49:35 -0600

    with expression update

--- with-scope.exs
+++ with-scope.exs
@@ -4,9 +4,17 @@ lp = with {:ok, file}     = File.open("/etc/passwd", [:read]),
           content         = IO.read(file, :all),
             :ok           = File.close(file),
             [_, uid, gid] = Regex.run(~r/fred:.*?:(\d+):(\d+)/, content)
+  do "Group: #{gid}, User: #{uid}"
+end
+
+lp2 = with {:ok, file}     = File.open("/etc/passwd", [:read]),
+           content         = IO.read(file, :all),
+             :ok           = File.close(file),
+             [_, uid, gid] <- Regex.run(~r/xxx:.*?:(\d+):(\d+)/, content)
   do
   "Group: #{gid}, User: #{uid}"
 end
 
-  IO.puts lp
-  IO.puts content
+IO.puts lp
+IO.puts inspect(lp2)
+IO.puts content
