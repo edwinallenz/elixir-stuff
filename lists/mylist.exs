ea235975be1a7016436f79a4fd48edfc847169e1
Fred Allen <edwin.allen@gmail.com>
2017-03-26 18:46:52 -0600

    Add more functions

--- lists/mylist.exs
+++ lists/mylist.exs
@@ -6,4 +6,10 @@ defmodule MyList do
   def square([]), do: []
   def square([head | tail]), do: [ head*head | square(tail)]
 
+  def add_1([]), do: []
+  def add_1([head|tail]), do: [ head+1 | add_1(tail)]
+
+  def sum([]), do: 0
+  def sum([ head | tail]), do: head + sum(tail)
+
 end
